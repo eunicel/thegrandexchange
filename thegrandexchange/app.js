1f79a6ccfeef594b8edeaa1a33ba34d618bfe9bd
Ami Suzuki <asuzuki@mit.edu>
2014-11-13 19:55:38 -0500

    app.js edit

--- thegrandexchange/app.js
+++ thegrandexchange/app.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var connection_string = 'mongodb://localhost/thegrandexchange';
+
+// if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+//   connection_string = 'mongodb://'+process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
+//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
+//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/'+
+//         process.env.OPENSHIFT_APP_NAME;
+// }
+
+mongoose.connect(connection_string);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Mongoose connection error:'));
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +29,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -21,6 +38,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(session({secret: 'secrets',user: ''}));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -56,5 +75,7 @@ app.use(function(err, req, res, next) {
     });
 });
 
+// var port = process.env.OPENSHIFT_NODEJS_PORT;
+// var ip = process.env.OPENSHIFT_NODEJS_IP;
 
 module.exports = app;
