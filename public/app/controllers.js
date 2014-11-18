31edc3131bfe3fd63f1c90d412433666dd6db8b7
Eunice Lin <eunicel@mit.edu>
2014-11-18 16:14:22 -0500

    connecting completed transactions to database

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -99,94 +99,11 @@ angular.module('thegrandexchange')
   '$location',
   'users',
   'session',
-  function($http, $scope, $location, session) {
-
-    var transactions = users.get(id).transactions;
-    var date = new Date();
-    var clicker = {
-      name: 'clicker',
-      description: 'click click click'
-    }
-    var eunice = {
-      firstName: 'Eunice',
-      lastName: 'Lin',
-      email: 'eunicel@mit.edu',
-      password: 'asdf'
-    }
-    var jeffrey = {
-      firstName: 'Jeffrey',
-      lastName: 'Sun',
-      email: 'jeffrey@mit.edu',
-      password: 'asdf'
-    }
-    var george = {
-      firstName: 'George',
-      lastName: 'Du',
-      email: 'gdu@mit.edu',
-      password: 'asdf'
-    }
-    var ami = {
-      firstName: 'Ami',
-      lastName: 'Suzuki',
-      email: 'ami@mit.edu',
-      password: 'asdf'
-    }
-
-    $scope.transactions = [
-    {
-      buyOffer: {
-        postedBy: eunice,
-        item: clicker,
-        postedAt: date,
-        price: 4,
-        type: 'buy'
-      },
-      sellOffer: {
-        postedBy: ami,
-        item: clicker,
-        postedAt: date,
-        price: 3,
-        type: 'sell'
-      },
-      item: clicker,
-      price: 3
-    },
-    {
-      buyOffer: {
-        postedBy: george,
-        item: clicker,
-        postedAt: date,
-        price: 4,
-        type: 'buy'
-      },
-      sellOffer: {
-        postedBy: eunice,
-        item: clicker,
-        postedAt: date,
-        price: 3,
-        type: 'sell'
-      },
-      item: clicker,
-      price: 6
-    },
-    {
-      buyOffer: {
-        postedBy: jeffrey,
-        item: clicker,
-        postedAt: date,
-        price: 4,
-        type: 'buy'
-      },
-      sellOffer: {
-        postedBy: eunice,
-        item: clicker,
-        postedAt: date,
-        price: 3,
-        type: 'sell'
-      },
-      item: clicker,
-      price: 6
-    }]
+  function($http, $scope, $location, users, session) {
+    users.get(session.name()._id).then(function (response) {
+      var user = response.data.user;
+      $scope.transactions = user.transactions;
+    });
     $scope.isBuyer = function(transaction){
       if(transaction.buyOffer.postedBy.email === session.name().username){
         return true;
@@ -248,29 +165,100 @@ angular.module('thegrandexchange')
   '$http',
   '$scope',
   '$location',
-  '$stateParams',
   'session',
-  'items',
-  function($http, $scope, $location, $stateParams, session, items) {
-    $scope.order = 'price';
-    
-    items.get($stateParams.id).then(function(response) {
-      $scope.item = response.data.item;
-    });
-    $scope.offer = function(type) {
-      // type = 'buy' or 'sell'
-      items.postOffer($scope.item._id, {
-        postedBy: session.username,
-        item: $scope.item._id,
-        postedAt: Date.now(),
-        price: $scope.price,
-        type: type
-      }).then(function(response) {
-        console.log(response);
-      }, function(error) {
-        console.log(error);
-      });
+  function($http, $scope, $location, session) {
+    var date = new Date();
+    var clicker = {
+      name: 'clicker',
+      description: 'click click click'
     }
+    var eunice = {
+      firstName: 'Eunice',
+      lastName: 'Lin',
+      email: 'eunicel@mit.edu',
+      password: 'asdf'
+    }
+    var jeffrey = {
+      firstName: 'Jeffrey',
+      lastName: 'Sun',
+      email: 'jeffrey@mit.edu',
+      password: 'asdf'
+    }
+    var george = {
+      firstName: 'George',
+      lastName: 'Du',
+      email: 'gdu@mit.edu',
+      password: 'asdf'
+    }
+    var ami = {
+      firstName: 'Ami',
+      lastName: 'Suzuki',
+      email: 'ami@mit.edu',
+      password: 'asdf'
+    }
+    $scope.item = {
+      name: 'Clicker',
+      description: 'yada yada yada yada yada yada yada yada yada yada yada yada',
+      offers: [
+      {
+        postedBy: eunice,
+        item: clicker,
+        postedAt: date,
+        price: 3.5,
+        type: 'buy'
+      },
+      {
+        postedBy: ami,
+        item: clicker,
+        postedAt: date,
+        price: 5,
+        type: 'buy'
+      },
+      {
+        postedBy: george,
+        item: clicker,
+        postedAt: date,
+        price: 4,
+        type: 'buy'
+      },
+      {
+        postedBy: jeffrey,
+        item: clicker,
+        postedAt: date,
+        price: 3,
+        type: 'buy'
+      },
+      {
+        postedBy: eunice,
+        item: clicker,
+        postedAt: date,
+        price: 4,
+        type: 'sell'
+      },
+      {
+        postedBy: jeffrey,
+        item: clicker,
+        postedAt: date,
+        price: 10,
+        type: 'sell'
+      },
+      {
+        postedBy: george,
+        item: clicker,
+        postedAt: date,
+        price: 100,
+        type: 'sell'
+      },
+      {
+        postedBy: ami,
+        item: clicker,
+        postedAt: date,
+        price: 4,
+        type: 'sell'
+      }]
+    }
+    $scope.order = 'price';
+
   }
 ])
 
