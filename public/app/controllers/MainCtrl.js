e53e9e982fc6a99a481c37b1d5d5c3134f0cb401
Eunice Lin <eunicel@mit.edu>
2014-11-18 21:58:26 -0500

    display best buy and sell price in marketplace

--- public/app/controllers/MainCtrl.js
+++ public/app/controllers/MainCtrl.js
@@ -18,16 +18,26 @@ angular.module('thegrandexchange')
       $location.url('items/'+ item._id);
     }
     items.getAll().success(function(response) {
+      $scope.items = response.items;
       if (response.success === true) {
-        var items = response.items;
-        console.log(items);
-        for (var i = 0; i < items.length;i++){
-          var item = items[i];
-          console.log(item);
+        for (var i = 0; i < $scope.items.length; i++){
+          var item = $scope.items[i];
           var offers = item.offers;
-          console.log(offers);
+          var buyPrices = [];
+          var sellPrices = [];
+          for (var j = 0; j < offers.length; j++){
+            var offer = offers[j];
+            var price = offer.price;
+            if(offer.type === 'buy'){
+              buyPrices.push(price);
+            } else if (offer.type === 'sell') {
+              sellPrices.push(price);
+            }
+          }
+          item.bestBuy = Math.max.apply(null, buyPrices);
+          item.bestSell = Math.min.apply(null, sellPrices);
         }
-        $scope.items = response.items;
+
       }
       else {
         $scope.items = [{name:'no items found', description:'rekt'}];
