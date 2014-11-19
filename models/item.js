33bf8c80593928dd9f6f4f5fed7b1ba951dc7ed3
Eunice Lin <eunicel@mit.edu>
2014-11-18 23:49:25 -0500

    fixed delete

--- models/item.js
+++ models/item.js
@@ -224,11 +224,13 @@ itemSchema.statics.removeOfferFromItemAndUser = function(item_id, offer_id, call
 // DELETE - delete offer
 itemSchema.statics.deleteOffer = function(userid, item_id, offer_id, callback) {
   Offer.findOne({_id: offer_id}, function(err, offer) {
-    if (userid === offer.postedBy) {
+    if (userid.toString() == offer.postedBy.toString()) {
+      console.log('same');
       Offer.findOneAndRemove({_id: offer_id}, function(err, offer) {
         utils.handleError(err);
+        console.log(offer);
         callback(offer);
-      });      
+      });
     } else {
       callback("Must be user who posted offer.");
     }
