7d1763f8f5a3ee17a0883b44486fbc319e6d3ad5
Eunice Lin <eunicel@mit.edu>
2014-12-02 16:39:51 -0500

    added alerts for when offer is posted or matched

--- public/app/controllers/ItemCtrl.js
+++ public/app/controllers/ItemCtrl.js
@@ -29,8 +29,12 @@ angular.module('thegrandexchange')
       };
       items.postOffer($scope.item._id, newOffer).then(function(response) {
         $scope.message = undefined;
+        $scope.error = false;
+        $scope.posted = false;
+        $scope.matched = false;
+
+        // posted
         if (response.data.message === 'No match') {
-          // console.log(session.name());
           newOffer.postedBy = {
             firstName: session.name().firstName,
             lastName: session.name().lastName,
@@ -39,12 +43,21 @@ angular.module('thegrandexchange')
           $scope.item.offers.push(newOffer);
           $scope.price = '';
           $scope.reputation = '';
+          $scope.posted = true;
+          $scope.message = 'Your offer has been posted.'
         }
+        // error
         else if (response.data.success === false) {
+          $scope.error = true;
           $scope.message = response.data.message;
         }
+        // matched
         else {
+          $scope.matched = true;
+          $scope.message = 'Your offer has been matched. Check your completed transaction or check your email for more information.'
           var offers = $scope.item.offers;
+          $scope.price = '';
+          $scope.reputation = '';
           for (var i = 0; i < offers.length; i++) {
             if (offers[i].price === response.data.transaction.price) {
               offers.splice(i, 1);
--- public/thegrandexchange.js
+++ public/thegrandexchange.js
@@ -183,12 +183,6 @@ $(document).ready(function() {
 
     $scope.review = function(transaction) {
       var review_score = transaction.score;
-      // completed if checkbox is checked
-      /*if(transaction.completed) {
-        review_score = 1;
-      } else {
-        review_score = -1;
-      }*/
       var newReview = {
         text: transaction.review_content,
         score: transaction.score
@@ -239,8 +233,12 @@ $(document).ready(function() {
       };
       items.postOffer($scope.item._id, newOffer).then(function(response) {
         $scope.message = undefined;
+        $scope.error = false;
+        $scope.posted = false;
+        $scope.matched = false;
+
+        // posted
         if (response.data.message === 'No match') {
-          // console.log(session.name());
           newOffer.postedBy = {
             firstName: session.name().firstName,
             lastName: session.name().lastName,
@@ -249,12 +247,21 @@ $(document).ready(function() {
           $scope.item.offers.push(newOffer);
           $scope.price = '';
           $scope.reputation = '';
+          $scope.posted = true;
+          $scope.message = 'Your offer has been posted.'
         }
+        // error
         else if (response.data.success === false) {
+          $scope.error = true;
           $scope.message = response.data.message;
         }
+        // matched
         else {
+          $scope.matched = true;
+          $scope.message = 'Your offer has been matched. Check your completed transaction or check your email for more information.'
           var offers = $scope.item.offers;
+          $scope.price = '';
+          $scope.reputation = '';
           for (var i = 0; i < offers.length; i++) {
             if (offers[i].price === response.data.transaction.price) {
               offers.splice(i, 1);
